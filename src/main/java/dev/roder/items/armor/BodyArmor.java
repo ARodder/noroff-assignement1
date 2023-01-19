a17317394c558873c781a812aa03bf85d968ecb9
Aleksander Røder <fylling8@gmail.com>
2023-01-19 14:50:30 +0100

    add: comment to body armor constructor

--- src/main/java/dev/roder/items/armor/BodyArmor.java
+++ src/main/java/dev/roder/items/armor/BodyArmor.java
@@ -10,6 +10,18 @@ import dev.roder.items.types.SlotType;
  * creates an Armor piece with body as the SlotType
  */
 public class BodyArmor extends Armor{
+    /**
+     * This is the constructor for the BodyArmor class which is a sub class of
+     * Armor.
+     * It takes in 4 parameters and assigns them to the instance variables.
+     * It calls the super class' constructor to set the name, slot, required level,
+     * armor type and armor attribute.
+     * 
+     * @param name           The name of the Body Armor
+     * @param requiredLevel  the level the player must be to use the Body Armor
+     * @param armorType      The type of the Body Armor
+     * @param armorAttribute The attribute of the Body Armor
+     */
     public BodyArmor(String name, int requiredLevel, ArmorType armorType, HeroAttribute armorAttribute) {
         super(name, SlotType.Body, requiredLevel, armorType, armorAttribute);
     }
